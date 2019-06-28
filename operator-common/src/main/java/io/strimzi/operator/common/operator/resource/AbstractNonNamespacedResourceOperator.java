a00658428c69c9a937caa735e4646c96c65477c5
Tom Bentley <tbentley@redhat.com>
2019-06-28 11:33:45 +0100

    Try to avoid test failure
    [ERROR] testFullCycle(io.strimzi.operator.common.operator.resource.RoleBindingOperatorIT)  Time elapsed: 0.27 s  <<< ERROR!
    io.strimzi.operator.common.operator.resource.TimeoutException: observe deletion of RoleBinding my-test-namespace/my-test-resource
    	at io.strimzi.operator.common.operator.resource.ResourceSupport$1.lambda$new$0(ResourceSupport.java:124)
    	at io.vertx.core.impl.VertxImpl$InternalTimerHandler.handle(VertxImpl.java:911)
    	at io.vertx.core.impl.VertxImpl$InternalTimerHandler.handle(VertxImpl.java:875)
    	at io.vertx.core.impl.ContextImpl.executeTask(ContextImpl.java:320)
    	at io.vertx.core.impl.EventLoopContext.execute(EventLoopContext.java:43)
    	at io.vertx.core.impl.ContextImpl.executeFromIO(ContextImpl.java:188)
    	at io.vertx.core.impl.ContextImpl.executeFromIO(ContextImpl.java:180)
    	at io.vertx.core.impl.VertxImpl$InternalTimerHandler.run(VertxImpl.java:901)
    	at io.netty.util.concurrent.PromiseTask$RunnableAdapter.call(PromiseTask.java:38)
    	at io.netty.util.concurrent.ScheduledFutureTask.run(ScheduledFutureTask.java:127)
    	at io.netty.util.concurrent.AbstractEventExecutor.safeExecute(AbstractEventExecutor.java:163)
    	at io.netty.util.concurrent.SingleThreadEventExecutor.runAllTasks(SingleThreadEventExecutor.java:404)
    	at io.netty.channel.nio.NioEventLoop.run(NioEventLoop.java:495)
    	at io.netty.util.concurrent.SingleThreadEventExecutor$5.run(SingleThreadEventExecutor.java:905)
    	at io.netty.util.concurrent.FastThreadLocalRunnable.run(FastThreadLocalRunnable.java:30)
    	at java.base/java.lang.Thread.run(Thread.java:834)
    
    Jun 27, 2019 5:27:14 PM io.vertx.core.impl.ContextImpl
    SEVERE: Unhandled exception
    io.strimzi.operator.common.operator.resource.TimeoutException: observe deletion of RoleBinding my-test-namespace/my-test-resource
    	at io.strimzi.operator.common.operator.resource.ResourceSupport$1.lambda$new$0(ResourceSupport.java:124)
    	at io.vertx.core.impl.VertxImpl$InternalTimerHandler.handle(VertxImpl.java:911)
    	at io.vertx.core.impl.VertxImpl$InternalTimerHandler.handle(VertxImpl.java:875)
    	at io.vertx.core.impl.ContextImpl.executeTask(ContextImpl.java:320)
    	at io.vertx.core.impl.EventLoopContext.execute(EventLoopContext.java:43)
    	at io.vertx.core.impl.ContextImpl.executeFromIO(ContextImpl.java:188)
    	at io.vertx.core.impl.ContextImpl.executeFromIO(ContextImpl.java:180)
    	at io.vertx.core.impl.VertxImpl$InternalTimerHandler.run(VertxImpl.java:901)
    	at io.netty.util.concurrent.PromiseTask$RunnableAdapter.call(PromiseTask.java:38)
    	at io.netty.util.concurrent.ScheduledFutureTask.run(ScheduledFutureTask.java:127)
    	at io.netty.util.concurrent.AbstractEventExecutor.safeExecute(AbstractEventExecutor.java:163)
    	at io.netty.util.concurrent.SingleThreadEventExecutor.runAllTasks(SingleThreadEventExecutor.java:404)
    	at io.netty.channel.nio.NioEventLoop.run(NioEventLoop.java:495)
    	at io.netty.util.concurrent.SingleThreadEventExecutor$5.run(SingleThreadEventExecutor.java:905)
    	at io.netty.util.concurrent.FastThreadLocalRunnable.run(FastThreadLocalRunnable.java:30)
    	at java.base/java.lang.Thread.run(Thread.java:834)

--- operator-common/src/main/java/io/strimzi/operator/common/operator/resource/AbstractNonNamespacedResourceOperator.java
+++ operator-common/src/main/java/io/strimzi/operator/common/operator/resource/AbstractNonNamespacedResourceOperator.java
@@ -106,7 +106,7 @@ public abstract class AbstractNonNamespacedResourceOperator<C extends Kubernetes
                     if (current != null) {
                         // Deletion is desired
                         log.debug("{} {} exist, deleting it", resourceKind, name);
-                        internalDelete(name).setHandler(future);
+                        observedDelete(name).setHandler(future);
                     } else {
                         log.debug("{} {} does not exist, noop", resourceKind, name);
                         future.complete(ReconcileResult.noop(null));
@@ -121,12 +121,14 @@ public abstract class AbstractNonNamespacedResourceOperator<C extends Kubernetes
     }
 
     /**
-     * Asynchronously deletes the resource with the given {@code name}.
+     * Asynchronously deletes the resource with the given {@code name},
+     * returning a Future which completes once the resource
+     * is observed to have been deleted.
      * @param name The resource to be deleted.
      * @return A future which will be completed on the context thread
      * once the resource has been deleted.
      */
-    private Future<ReconcileResult<T>> internalDelete(String name) {
+    private Future<ReconcileResult<T>> observedDelete(String name) {
         R resourceOp = operation().withName(name);
         Future<ReconcileResult<T>> watchForDeleteFuture = resourceSupport.selfClosingWatch(resourceOp,
             "observe deletion of " + resourceKind + " " + name,
@@ -139,7 +141,31 @@ public abstract class AbstractNonNamespacedResourceOperator<C extends Kubernetes
                 }
             });
         Future<Void> deleteFuture = resourceSupport.deleteAsync(resourceOp);
-        return CompositeFuture.join(watchForDeleteFuture, deleteFuture).map(ReconcileResult.deleted());
+        return CompositeFuture.join(watchForDeleteFuture, deleteFuture)
+                .map(ReconcileResult.<T>deleted())
+                .recover(error -> {
+                    // Belt-and-braces: If we're about to time out, double-check whether the resource
+                    // exists before propagating the TimeoutException
+                    if (error instanceof TimeoutException) {
+                        Future<ReconcileResult<T>> f = Future.future();
+                        resourceSupport.getAsync(resourceOp)
+                            .setHandler(ar -> {
+                                if (ar.succeeded()) {
+                                    if (ar.result() == null) {
+                                        f.complete(ReconcileResult.deleted());
+                                    } else {
+                                        f.fail(error);
+                                    }
+                                } else {
+                                    error.addSuppressed(ar.cause());
+                                    f.fail(error);
+                                }
+                            });
+                        return f;
+                    } else {
+                        return Future.failedFuture(error);
+                    }
+                });
     }
 
     /**
